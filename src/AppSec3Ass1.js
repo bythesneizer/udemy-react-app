1fdb0d453417954d99bfe12404c6ce963dd413c1
Ryan Bytheway MAC-VM <ryan.bytheway@engineering.digital.dwp.gov.uk>
2019-06-13 15:09:47 +0100

    added components and app.js for section 3 assignment 1

---
+++ src/AppSec3Ass1.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import UserOutput from './UserOutput/UserOutput'
+import UserInput from './UserInput/UserInput'
+import './App.css';
+
+class App extends Component {
+  
+  state = {
+    username: 'InitialUsername'
+  }
+
+  usernameHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+       <h1>React Application</h1>
+       <p>This is the app for Section 3 Assignment 1</p>
+       <UserInput
+          change={this.usernameHandler}
+          username={this.state.username}
+       ></UserInput>
+       <UserOutput 
+          username={this.state.username}
+          p2='Pargraph for UserOutput Componenet'
+        ></UserOutput>
+        <UserOutput 
+          username={this.state.username}
+          p2='Pargraph for UserOutput Componenet'
+        ></UserOutput>
+      </div>
+    )
+  }
+}
+
+export default App;
